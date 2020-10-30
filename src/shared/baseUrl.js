45bfda03b7064f2bd8a5f89abdc24d3ea1b940f3
olehd <harmash853@gmail.com>
2020-10-30 23:05:34 +0200

    Updates

--- src/shared/baseUrl.js
+++
@@ -1,3 +0,0 @@
-export const allPokemons = 'https://pokeapi.co/api/v2/pokemon?limit=';
-export const typesOfPokemons = '';
-export const imageOfPokemon = 'https://pokeres.bastionbot.org/images/pokemon/'; //in the end add .png
